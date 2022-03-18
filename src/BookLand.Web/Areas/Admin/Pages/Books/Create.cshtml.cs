39c40b263e67a1669539a3be0081bc226103e89a
Hakim Ghods <ghods.hakim@hotmail.com>
2022-03-18 11:58:38 +0330

    upload images

--- src/BookLand.Web/Areas/Admin/Pages/Books/Create.cshtml.cs
+++ src/BookLand.Web/Areas/Admin/Pages/Books/Create.cshtml.cs
@@ -8,21 +8,38 @@ namespace BookLand.Web.Areas.Admin.Pages.Books
     public class CreateModel : PageModel
     {
         private readonly BookLandDbContext _db;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public CreateModel(BookLandDbContext db)
+        public CreateModel(BookLandDbContext db, IWebHostEnvironment webHostEnvironment)
         {
             _db = db;
-            Book=new Book
+            Book = new Book
             {
-                Year=0,
-                Pages=1
+                Year = 0,
+                Pages = 1
             };
+            _webHostEnvironment = webHostEnvironment;
         }
 
         [BindProperty]
         public Book? Book { get; set; }
-        public IActionResult OnPost()
+
+        [BindProperty]
+        public IFormFile ImageFile { get; set; }
+        public async Task<IActionResult> OnPost()
         {
+            var fileName= Book.Title.ToLower().Replace(" ", " - ") + ".jpg";
+            if (ImageFile!=null)
+            {
+                var path = _webHostEnvironment.WebRootPath + @"\images\" + fileName;
+                
+                using var stream = System.IO.File.Create(path);
+                stream.Position = 0;
+                await ImageFile.CopyToAsync(stream);
+                // Copy to images folder
+            }
+            Book.ImageLink = "images/" + fileName;
+
             if (ModelState.IsValid==false)
             {
                 return Page();
