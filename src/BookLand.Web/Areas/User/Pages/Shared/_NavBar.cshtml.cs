e8216d1042fc439f40a74e988b1dca4c9b14cc88
Hakim Ghods <ghods.hakim@hotmail.com>
2022-03-23 12:19:31 +0430

    navbar

---
+++ src/BookLand.Web/Areas/User/Pages/Shared/_NavBar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookLand.Web.Areas.User.Pages.Shared
+{
+    public class _NavBarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
