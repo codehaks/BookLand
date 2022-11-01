d6ba902357db316012667e8a609a54ac73c0ec4b
Hakim Ghods <ghods.hakim@hotmail.com>
2022-11-01 11:24:42 +0330

    fix

---
+++ src/BookLand.Web/ViewComponents/ImageBox.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookLand.Web.ViewComponents;
+
+[ViewComponent]
+public class ImageBox : ViewComponent
+{
+    public ImageBoxViewModel ViewModel { get; set; }
+    public async Task<IViewComponentResult> InvokeAsync(byte[] content,string style,string alt)
+    {
+        ViewModel=new ImageBoxViewModel();
+
+        ViewModel.ImageBase64=Convert.ToBase64String(content);
+        ViewModel.Style = style;
+        ViewModel.Alt = alt;
+        return View("Default",ViewModel); 
+    }
+}
+
+public class ImageBoxViewModel
+{
+    public string ImageBase64 { get; set; }
+    public string Style { get; set; }
+    public string Alt { get; set; }
+}
