aafe5f93137d58397e0415cff8dc86b9d89d1589
Hakim Ghods <ghods.hakim@hotmail.com>
2022-11-19 20:14:54 +0330

    Query Book List

--- src/BookLand.Web/Areas/Admin/Pages/Books/Index.cshtml.cs
+++ src/BookLand.Web/Areas/Admin/Pages/Books/Index.cshtml.cs
@@ -1,5 +1,8 @@
+using BookLand.Application.Books.Queries;
 using BookLand.Data;
 using BookLand.Models;
+using Mapster;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -9,74 +12,36 @@ namespace BookLand.Web.Areas.Admin.Pages.Books;
 
 public class IndexModel : PageModel
 {
-    private readonly BookLandDbContext _db;
-
-    public IndexModel(BookLandDbContext db)
+    private readonly IMediator _mediator;
+    public IndexModel( IMediator mediator)
     {
-        _db = db;
+        _mediator = mediator;
     }
 
-    public IList<Book> BookList { get; set; }
+    //public IList<Book> BookList { get; set; }
+    public IList<BookViewModel> BookList { get; set; }
 
     public string Term { get; set; }
     public string SortBy { get; set; }
 
     [BindProperty]
     public string SelectedLanguage { get; set; }
-    public void OnGet(string term = "", string orderBy = "author", string sortBy = "asc",string selectedLanguage="All")
+    public async Task<IActionResult> OnGet(string term = "", string orderBy = "author", string sortBy = "asc", string selectedLanguage = "All")
     {
-        IQueryable<Book> booksQuery = _db.Books.Include(b=>b.Category);
-
-        booksQuery = OrderBooks(orderBy, sortBy, booksQuery);
-        sortBy = GetSortBy(sortBy);
-        SortBy = sortBy;
-
-        if (string.IsNullOrEmpty(term) == false)
+        var result = await _mediator.Send(new FindAllBooks.Query
         {
-            booksQuery = booksQuery
-                .Where(b => b.Title.ToLower()
-                .Contains(term.ToLower()));
-        }
+            Term = term,
+            SortBy=sortBy,
+            SelectedLanguage=selectedLanguage,
+            OrderBy=orderBy
 
-        if (string.IsNullOrEmpty(selectedLanguage) ==false && selectedLanguage != "All")
-        {
-            booksQuery=booksQuery.Where(b=>b.Language== selectedLanguage);
-        }
+        });
 
-        BookList = booksQuery.ToList();
+        BookList = result.Books.Adapt<IList<BookViewModel>>();
         Term = term;
+        SortBy = GetSortBy(sortBy);
         SelectedLanguage = selectedLanguage;
-
-    }
-
-    private static IQueryable<Book> OrderBooks(string orderBy, string sortBy, IQueryable<Book> booksQuery)
-    {
-        if (orderBy == "year")
-        {
-            if (sortBy == "asc")
-            {
-                booksQuery = booksQuery.OrderBy(b => b.Year);
-            }
-            else
-            {
-                booksQuery = booksQuery.OrderByDescending(b => b.Year);
-            }
-        }
-
-        if (orderBy == "author")
-        {
-
-            if (sortBy == "asc")
-            {
-                booksQuery = booksQuery.OrderBy(b => b.Author);
-            }
-            else
-            {
-                booksQuery = booksQuery.OrderByDescending(b => b.Author);
-            }
-        }
-
-        return booksQuery;
+        return Page();
     }
 
     private static string GetSortBy(string sortBy)
@@ -92,4 +57,28 @@ public class IndexModel : PageModel
 
         return sortBy;
     }
+
+
+}
+
+
+public class BookViewModel
+{
+    public int Id { get; set; }
+
+    public Category? Category { get; set; }
+
+    public string Title { get; set; } = default!;
+
+    public int Price { get; set; }
+
+    public string Author { get; set; } = default!;
+
+    public int Year { get; set; } = default;
+
+    public int Pages { get; set; } = 1;
+
+    public string ImageLink { get; set; } = default!;
+
+    public string Link { get; set; } = default!;
 }
---
+++ src/BookLand/Application/Books/Queries/FindAllBooks.cs
@@ -0,0 +1,118 @@
+﻿using BookLand.Data;
+using BookLand.Models;
+using Mapster;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+
+namespace BookLand.Application.Books.Queries;
+
+public class FindAllBooks
+{
+    public class Query : IRequest<Response>
+    {
+        public int Id { get; set; }
+        public string Term { get; set; }
+
+        public string SortBy { get; set; }
+
+        public string SelectedLanguage { get; set; }
+
+        public string OrderBy { get; set; }
+    }
+
+    public class Handler : IRequestHandler<Query, Response>
+    {
+        private readonly BookLandDbContext _db;
+
+        public Handler(BookLandDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<Response> Handle(Query query, CancellationToken cancellationToken)
+        {
+            IQueryable<Book> booksQuery = _db.Books.Include(b => b.Category);
+            booksQuery = OrderBooks(query.OrderBy, query.SortBy, booksQuery);
+            
+            if (string.IsNullOrEmpty(query.Term) == false)
+            {
+                booksQuery = booksQuery
+                    .Where(b => b.Title.ToLower()
+                    .Contains(query.Term.ToLower()));
+            }
+
+            if (string.IsNullOrEmpty(query.SelectedLanguage) == false && query.SelectedLanguage != "All")
+            {
+                booksQuery = booksQuery.Where(b => b.Language == query.SelectedLanguage);
+            }
+
+            var BookList = booksQuery
+                .ProjectToType<BookOutput>()
+                .ToList();
+
+            var response = new Response
+            {
+                Books = BookList
+            };
+            return response;
+        }
+
+        private static IQueryable<Book> OrderBooks(string orderBy, string sortBy, IQueryable<Book> booksQuery)
+        {
+            if (orderBy == "year")
+            {
+                if (sortBy == "asc")
+                {
+                    booksQuery = booksQuery.OrderBy(b => b.Year);
+                }
+                else
+                {
+                    booksQuery = booksQuery.OrderByDescending(b => b.Year);
+                }
+            }
+
+            if (orderBy == "author")
+            {
+
+                if (sortBy == "asc")
+                {
+                    booksQuery = booksQuery.OrderBy(b => b.Author);
+                }
+                else
+                {
+                    booksQuery = booksQuery.OrderByDescending(b => b.Author);
+                }
+            }
+
+            return booksQuery;
+        }
+    }
+
+    public class Response
+    {
+        public IList<BookOutput> Books { get; set; }
+    }
+}
+
+
+public class BookOutput
+{
+    public int Id { get; set; }
+
+    public Category? Category { get; set; }
+
+    public string Title { get; set; } = default!;
+
+    public int Price { get; set; }
+
+    public string Author { get; set; } = default!;
+
+    public int Year { get; set; } = default;
+
+    public int Pages { get; set; } = 1;
+
+    public string ImageLink { get; set; } = default!;
+
+    public string Link { get; set; } = default!;
+}
